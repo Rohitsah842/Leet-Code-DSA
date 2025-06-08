53c0508e80ed2bf34817f5acf8c72c4432a1cfcb
Rohit Kumar <84443409+Rohitsah842@users.noreply.github.com>
2025-06-08 10:17:41 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (54.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -13,33 +13,35 @@ class Solution {
         int carry=0;
         ListNode result=new ListNode(0);
         ListNode temp=result;
-        while(l1!=null && l2!=null){
-            int sum=l1.val+l2.val+carry;
+        while(l1!=null || l2!=null){
+            int val1=l1!=null? l1.val:0;
+            int val2=l2!=null? l2.val:0;
+            int sum=val1+val2+carry;
             temp.next=new ListNode(sum%10);
             temp=temp.next;
             carry=sum/10;
-            l1=l1.next;
-            l2=l2.next;
+            l1=l1!=null? l1.next:l1;
+            l2=l2!=null? l2.next:l2;
 
         }
-        if(l1!=null){
-            while(l1!=null){
-                 int sum=l1.val+carry;
-           temp.next=new ListNode(sum%10);
-            temp=temp.next;
-            carry=sum/10;
-            l1=l1.next;
-            }
-        }
-        else{
-            while(l2!=null){
-                 int sum=l2.val+carry;
-             temp.next=new ListNode(sum%10);
-            temp=temp.next;
-            carry=sum/10;
-            l2=l2.next;
-            }
-        }
+        // if(l1!=null){
+        //     while(l1!=null){
+        //          int sum=l1.val+carry;
+        //    temp.next=new ListNode(sum%10);
+        //     temp=temp.next;
+        //     carry=sum/10;
+        //     l1=l1.next;
+        //     }
+        // }
+        // else{
+        //     while(l2!=null){
+        //          int sum=l2.val+carry;
+        //      temp.next=new ListNode(sum%10);
+        //     temp=temp.next;
+        //     carry=sum/10;
+        //     l2=l2.next;
+        //     }
+        // }
         if(carry!=0){
             temp.next=new ListNode(carry);
             temp=temp.next;
